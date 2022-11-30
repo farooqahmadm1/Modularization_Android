aa4d2952e43bf37b9e79821626c3e3273bcf9185
farooqeddress <farooqahmadm1@gmail.com>
2022-11-30 10:43:33 +0500

    updates

--- buildsrc/src/main/java/Config.kt
+++ buildsrc/src/main/java/Config.kt
@@ -10,11 +10,6 @@ object Config {
         const val release = "release"
     }
 
-    object Keys {
-        const val API_KEY_NAME = "API_KEY"
-        const val API_KEY_VALUE = "\"ccebae2c458930bb2bdd06685da41ec3\""
-    }
-
     const val STRING = "String"
     const val BASE_URL = "BASE_URL"
 }
